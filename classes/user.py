a33988b88e65b5c0555511b51ec3deae566176e5
Jose Candia <josecandiap@gmail.com>
2023-05-03 11:42:47 -0400

    fix: solicion error obtener usuarios

--- classes/user.py
+++ classes/user.py
@@ -25,7 +25,7 @@ class UserController:
         self.file_manager = FileManager('users.json')
         
     def get_users(self):
-        returned_list = self.file_manager.get_registers()
+        returned_list = self.file_manager.get_registers(is_print_exception=True)
         UserController.user_list = returned_list;
         
     def save_user(self, name:str, last_name:str, email:str, password:str):
@@ -33,9 +33,9 @@ class UserController:
         self.file_manager.save(new_user.to_dict())
         
     def login(self, email:str, password:str) -> UserModel | None:
-        UserController.get_users()
-        for user in UserController.user_list:
-            if user.email == email and user.passwod == password:
+        UserController().get_users()
+        for user in UserController().user_list:
+            if user['email'] == email and user['password'] == password:
                 return user
         return None
     
@@ -50,6 +50,8 @@ class UserView:
             email = input('Ingrese su correo electronico:')
             password = input('Ingrese su contraseña:')
             UserController().save_user(name, last_name, email, password)
+            print(f'\nUsuario ${ name } registrado con exito!')
+            time.sleep(5)
         except Exception as e:
             print(f'Error al registar usuario: [{ e }]')
             time.sleep(3)
@@ -59,10 +61,28 @@ class UserView:
             print('*************************************')
             print('        Usuarios registrados:')
             print('*************************************\n')
-            lista_usuario = UserController.get_users(is_print_exception=True)
-            for index, user in enumerate(lista_usuario):
+            UserController().get_users()
+            for index, user in enumerate(UserController().user_list):
                 print(f'{ index + 1 }.- USUARIO [{ user["name"] }] / PASSWORD [{ user["password"] }]')
-            input('Presione una tecla para continuar...')
-        except:
-            print('No hay usuarios registrados :(...')
+            input('\nPresione la tecla [ENTER] para continuar...')
+        except Exception as e:
+            print('No hay usuarios registrados..')
+            time.sleep(3)
+            
+    def login():
+        try:
+            print('*************************************')
+            print('               Login:')
+            print('*************************************\n')
+            user_email = input('Ingrese el email: ')
+            user_password = input('Ingrese la contraseña: ')
+            user:UserModel = UserController().login(user_email, user_password)
+            if user == None:
+                print('\nCredenciales no validas!')
+                time.sleep(3)
+                return
+            print(f"\nUsuario logeado con exito! Bienvenido [{ user['name'] } { user['last_name'] }]")
+            time.sleep(3)
+        except Exception as e:
+            print(f'Error en login [{ e }]')
             time.sleep(3)
